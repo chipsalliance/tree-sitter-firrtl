922497cd3cbd9dab52fcb05349611470de2fff31
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2019-10-01 22:03:36 -0700

    fixed primop keyword bug ; split statement rules ; cleanup strings

--- grammar.js
+++ grammar.js
@@ -73,8 +73,8 @@ const rules = {
 
   defname: $ => seq('defname', '=', $.id, $._newline),
 
-  parameter: $ => seq(
-    'parameter', $.id, '=',
+  parameter: $ => seq('parameter', $.id,
+    '=',
     choice(
       $.intLit,
       $.StringLit,
@@ -99,28 +99,41 @@ const rules = {
   ),
 
   stmt: $ => choice(
-    seq(
-      choice('wire', 'cmem', 'smem'),
-      $.id, ':', $.type,
-      optional($.info)
-    ),
-    seq('reg',
-      $.id, ':', $.type, ',', $.exp, optseq('with', ':', $.reset_block), // no comma in ANTLR
-      optional($.info)
-    ),
-    seq('mem', $.id, ':', optional($.info), $._indent, repeat($.memField), $._dedent),
+    $.wire,
+    $.cmem,
+    $.smem,
+    $.reg,
+    $.mem,
     seq($.mdir, 'mport', $.id, '=', $.id, '[', $.exp, ']', ',', $.exp, optional($.info)),
-    seq('inst', $.id, 'of', $.id, optional($.info)),
-    seq('node', $.id, '=', $.exp, optional($.info)),
+    $.inst,
+    $.node,
     seq($.exp, '<=', $.exp, optional($.info)),
     seq($.exp, '<-', $.exp, optional($.info)),
     seq($.exp, 'is', 'invalid', optional($.info)),
     $.when,
-    seq('stop', '(', $.exp, ',', $.exp, ',', $.intLit, ')', optional($.info)),
-    seq('printf', '(', $.exp, ',', $.exp, ',', $.StringLit, repseq(',', $.exp), ')', optional($.info)),
-    seq('skip', optional($.info)),
-    seq('attach', '(', sep1(',', $.exp, ')'), optional($.info))
+    $.stop,
+    $.printf,
+    $.skip,
+    $.attach
   ),
+
+  wire: $ => seq('wire', $.id, ':', $.type, optional($.info)),
+
+  cmem: $ => seq('cmem', $.id, ':', $.type, optional($.info)),
+
+  smem: $ => seq('smem', $.id, ':', $.type, optional($.info)),
+
+  reg: $ => seq('reg', $.id, ':', $.type, ',', $.exp,
+    optseq('with', ':', $.reset_block), // no comma in ANTLR
+    optional($.info)
+  ),
+
+  mem: $ => seq('mem', $.id, ':', optional($.info),
+    $._indent, repeat($.memField), $._dedent),
+
+  inst: $ => seq('inst', $.id, 'of', $.id, optional($.info)),
+
+  node: $ => seq('node', $.id, '=', $.exp, optional($.info)),
 
   memField: $ => seq(
     choice(
@@ -140,8 +153,7 @@ const rules = {
     seq($._indent, repeat1($.simple_stmt), $._dedent)
   ),
 
-  when: $ => seq(
-    'when', $.exp, ':', optional($.info),
+  when: $ => seq('when', $.exp, ':', optional($.info),
     optional($.suite),
     optseq('else',
       choice($.when,
@@ -150,7 +162,15 @@ const rules = {
     )
   ),
 
-  info: $ =>  $.FileInfo,
+  stop: $ => seq('stop', '(', $.exp, ',', $.exp, ',', $.intLit, ')', optional($.info)),
+
+  printf: $ => seq('printf', '(', $.exp, ',', $.exp, ',', $.StringLit, repseq(',', $.exp), ')', optional($.info)),
+
+  skip: $ => seq('skip', optional($.info)),
+
+  attach: $ => seq('attach', '(', sep1(',', $.exp, ')'), optional($.info)),
+
+  info: $ =>  /@\[.*\]/, // $.FileInfo,
 
   mdir: $ => choice('infer', 'read', 'write', 'rdwr'),
 
@@ -166,16 +186,15 @@ const rules = {
     seq($.exp, '[', $.exp, ']'),
     seq('mux(', $.exp, ',', $.exp, ',', $.exp, ')'), // no commas in ANTLR
     seq('validif', '(', $.exp, ',', $.exp, ')'),
-    seq($.primop, '(', sep1(',', choice($.exp, $.intLit)), ')') // no commas in ANTLR
+    seq($.primop, sep1(',', choice($.exp, $.intLit)), ')') // no commas in ANTLR
   ),
 
-  id: $ => /[a-zA-Z_]\w*/,
+  // id: $ => $.Id, // /[a-zA-Z_]\w*/,
 
   fieldId: $ => choice(
-    $.id // FIXME
-    // $.Id,
-    // $.RelaxedId,
-    // $.UnsignedInt,
+    $.id, // $.Id,
+    $.RelaxedId,
+    $.UnsignedInt
     // $.keywordAsId
   ),
 
@@ -186,62 +205,67 @@ const rules = {
   ),
 
   primop: $ => choice(
-    'add',
-    'sub',
-    'mul',
-    'div',
-    'rem',
-    'lt',
-    'leq',
-    'gt',
-    'geq',
-    'eq',
-    'neq',
-    'pad',
-    'asUInt',
-    'asAsyncReset',
-    'asSInt',
-    'asClock',
-    'shl',
-    'shr',
-    'dshl',
-    'dshr',
-    'cvt',
-    'neg',
-    'not',
-    'and',
-    'or',
-    'xor',
-    'andr',
-    'orr',
-    'xorr',
-    'cat',
-    'bits',
-    'head',
-    'tail',
-    'asFixedPoint',
-    'bpshl',
-    'bpshr',
-    'bpset'
+    'add(',
+    'sub(',
+    'mul(',
+    'div(',
+    'rem(',
+    'lt(',
+    'leq(',
+    'gt(',
+    'geq(',
+    'eq(',
+    'neq(',
+    'pad(',
+    'asUInt(',
+    'asAsyncReset(',
+    'asSInt(',
+    'asClock(',
+    'shl(',
+    'shr(',
+    'dshl(',
+    'dshr(',
+    'cvt(',
+    'neg(',
+    'not(',
+    'and(',
+    'or(',
+    'xor(',
+    'andr(',
+    'orr(',
+    'xorr(',
+    'cat(',
+    'bits(',
+    'head(',
+    'tail(',
+    'asFixedPoint(',
+    'bpshl(',
+    'bpshr(',
+    'bpset('
   ),
 
 // Tokens
 
-  UnsignedInt: $ => choice('0', $.PosInt),
+  UnsignedInt: $ => choice('0', /[1-9][0-9]*/),
 
-  SignedInt: $ => seq(choice('+', '-'), $.PosInt),
+  SignedInt: $ => seq(choice('+', '-'), /[1-9][0-9]*/),
 
-  PosInt: $ => /[1-9][0-9]*/,
+  // PosInt: $ => /[1-9][0-9]*/,
 
   HexLit: $ => /"h[+-]?[a-fA-F0-9]+"/,
 
   DoubleLit: $ => /[+-]*[0-9]+\.[0-9]+(E[+-]?[0-9]+)?/,
 
-  StringLit: $ => seq('"', /([\x09\x20\x21\x23-\xFE]|\\")*/, '"'),
+  StringLit: $ => /"([\x09\x20\x21\x23-\xFE]|\\")*"/,
 
   RawString: $ => /'[^\\']*'/,
 
   FileInfo: $ => /@\[.*\]/,
+
+  // Id: $ => /[a-zA-Z_][a-zA-Z0-9$]*/, // LegalStartChar (LegalIdChar)*
+  id: $ => /[a-zA-Z_][a-zA-Z0-9_$]*/, // LegalStartChar (LegalIdChar)*
+
+  RelaxedId: $ => /[a-zA-Z0-9_$]+/, // (LegalIdChar)+
 
   comment: $ => token(
     seq(';', /.*/)
